0c609607aa2fdcf0cf018774e4abdab1a197b647
Amitava Basu <amitava.basu@gmail.com>
2019-08-20 20:56:52 -0500

    added a better method and added comment

--- src/com/amit/alogs/hr/easy/HR15MaxArraySumWithinAWindow.java
+++ src/com/amit/alogs/hr/easy/HR15MaxArraySumWithinAWindow.java
@@ -1,11 +1,33 @@
 package com.amit.alogs.hr.easy;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Stack;
 
 public class HR15MaxArraySumWithinAWindow {
+	/*
+	 * return max sum of a given window size within an array of integers
+	 * 
+	 */
 	public static int maxSubArraySumInAGivenWindow(int[] arr, int window){
+		
+		int sum = 0;
+		int i=0;
+		while(i<window){//<-- calculate sum of the window
+			sum = sum+arr[i];
+			i++;
+		}
+		int maxSum = sum;
+		while(i<arr.length) {//<-- iterate over rest of the array
+			sum = sum-arr[i-window]+arr[i];//<-- calculate next sum
+			if(maxSum<sum){//<-- check and reassign maximum sum
+				maxSum = sum;
+			}
+				i++;
+		}
+		return maxSum;//<-- return maximum sum
+	}
+	
+	public static int maxSubArraySumInAGivenWindowAlternateMethod(int[] arr, int window){
 		int maxSum = Integer.MIN_VALUE;
 		int sum = 0;
 		int i=0;
@@ -15,7 +37,6 @@ public class HR15MaxArraySumWithinAWindow {
 				sum = sum+arr[i];
 				i++;
 			}else{
-				System.out.println(sum);
 				if(sum>maxSum){
 					maxSum = sum;
 				}
@@ -24,12 +45,14 @@ public class HR15MaxArraySumWithinAWindow {
 				j++;
 			}
 		}
-		System.out.println(sum);
 		if(sum>maxSum){
 			maxSum = sum;
 		}
 		return maxSum;
 	}
+	
+	
+	
 	
 	static long largestRectangle(int[] h) {
 	    Stack<Integer[]> stack = new Stack<>();
@@ -57,12 +80,13 @@ public class HR15MaxArraySumWithinAWindow {
 	}
 	
 	public static void main(String[] args){
-//		int[] arr = {5,7,9,10,20,30,8,1};
-//		System.out.println(maxSubArraySumInAGivenWindow(arr, 3));
+		int[] arr = {5,7,9,10,20,30,8,1};
+		System.out.println(maxSubArraySumInAGivenWindowAlternateMethod(arr, 3));
+		System.out.println(maxSubArraySumInAGivenWindow(arr, 3));
 		//int[] h = {5,4,3,2,1};
 		//int[] h = {1,2,3,4,5};
 		//int[] h = {8979, 4570, 6436, 5083, 7780, 3269, 5400, 7579, 2324, 2116};
-		int[] h = {1,2,3,4,5,4,6,2,1};
-		System.out.println(largestRectangle(h));
+		//int[] h = {1,2,3,4,5,4,6,2,1};
+		//System.out.println(largestRectangle(h));
 	}
 }
